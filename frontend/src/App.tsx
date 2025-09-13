4d9306a18df3551a3ac6b5ead0f9ff3f0d954d2f
malingaray <jmalingaray@gmail.com>
2025-09-13 19:43:53 +0530

    initiate the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="font-bold">
+      Hello World
+      <Button variant="link">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
