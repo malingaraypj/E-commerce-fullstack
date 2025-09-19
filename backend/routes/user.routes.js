34ba19f7a9de640cc0209ec3ea68882796669421
malingaray <jmalingaray@gmail.com>
2025-09-19 14:33:04 +0530

    correct the middlewares for protecting

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,9 +4,16 @@ import {
   updateOneUser,
 } from "../controllers/user.controller.js";
 import { getOneUser } from "../controllers/admin.controller.js";
+import { restrictTo } from "../services/auth.services.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").patch(updateOneUser).get(getOneUser).delete(deleteOneUser);
+router
+  .route("/")
+  .all(protect)
+  .patch(updateOneUser)
+  .get(getOneUser)
+  .delete(restrictTo("admin"), deleteOneUser);
 
 export default router;
